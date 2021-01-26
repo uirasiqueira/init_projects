916b6734abc7d6fbc978f47ae503eac4b7c3496b
Uirá Oliveira <u.siqueira@hotmail.com>
2021-01-26 00:13:12 +0000

    Commit1

---
+++ Validacao de ID/Validacao.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+file1 = pd.read_excel(r'INSIRA AQUI O CAMINHO DO ARQUIVO EM SUA MÁQUINA')
+a = file1
+A = np.array(a)#Library Numpy utilizada para transformar os dados dos arquivo em Excel em um array
+
+chave = int(input('Digite o seu código da empresa: '))
+
+y=0
+while chave not in A[0]:
+    chave = int(input('Código inválido. Digite novamente: '))
+    y+=1
+
+print('ID válido.')
